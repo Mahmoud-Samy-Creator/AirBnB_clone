24438a1a87af633aab18e813eac6caa9d68449ca
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-01-09 08:54:05 -0500

    3. BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+"""
+A class BaseModelthat defines
+all common attributes/methods for other classes
+"""
+
+# to generate unique id [ Use it in string only ]
+from uuid import uuid4
+
+# datetime - assign with the current datetime when an instance is created
+from datetime import datetime
+
+
+class BaseModel:
+    def __init__(self):
+        """
+        Define public instance attributes
+        - id: unique id for every instance
+        - created_at: the time at which the instance created
+        - updated_at: the time at which the instance updated
+        """
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.update_at = datetime.now()
+
+    def __str__(self):
+        """
+        display string information about new instant
+        """
+        class_name = self.__class__.__name__
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """
+        updates the public instance attribute
+        updated_at with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        return a dictionary of all attributes information
+        """
+        class_name = self.__class__.__name__
+        formatted_created_at = self.created_at.isoformat()
+        formatted_updated_at = self.update_at.isoformat()
+
+        final_dict = {
+            "name": class_name,
+            "id": self.id,
+            "created_at": formatted_created_at,
+            "updated_at": formatted_updated_at
+        }
+
+        final_dict.update(self.__dict__)
+
+        return final_dict
